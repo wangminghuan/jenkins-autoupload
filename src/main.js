4a5dd824db5ed4860af295acaf11e7bef95fd0e4
wangminghuan <m.h.wang@foxmail.com>
2019-09-20 17:42:28 +0800

    vue project -1-1

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-console.log("1233454542");
+console.log("1234542");
 new Vue({
   render: h => h(App),
 }).$mount('#app')
