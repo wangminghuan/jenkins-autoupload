b325148b89848031738ecbffe469438a44cede12
wangminghuan <m.h.wang@foxmail.com>
2019-09-23 09:41:40 +0800

    vue project -1-17

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome  uuuuujjjj9999skdkjS to Your Vue.js App"/>
+    <HelloWorld msg="Welcome  uuuuujjjsdddd99skdkjS to Your Vue.js App"/>
   </div>
 </template>
 
