be4fd47a2177e55b78adf87537bb71b677120af4
wangminghuan <m.h.wang@foxmail.com>
2019-09-24 15:54:16 +0800

    V1.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Test Vue.js App "/>
+    <HelloWorld msg="Welcome to Your Test-1 Vue.js App "/>
   </div>
 </template>
 
